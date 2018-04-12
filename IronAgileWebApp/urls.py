f4fbb13517b766fa7ec3283ee4bd23ffccecb59f
Had <hhoutmann@gmail.com>
2018-04-12 10:44:31 +0200

    Urls et login
    Implémentation

---
+++ IronAgileWebApp/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf.urls import url, include
+
+from IronAgileWebApp.views import loginViews, registerViews
+
+urlpatterns = [
+
+    # Gestion de la session
+    url('login/', loginViews.user_login),
+    url('logout/', loginViews.logout_user, name='logout_user'),
+    url('register/', registerViews.register)
+
+
+    ]
