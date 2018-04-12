e25113e731303ca88774a3fbafa6f1b9151d0e83
Had <hhoutmann@gmail.com>
2018-04-12 10:20:39 +0200

    Modification gitignore

--- IronAgileWebApp/models.py
+++ IronAgileWebApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    """
+    Class pour la gestion des utilateurs
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    adresse = models.CharField(max_length=50)
+    codePostal = models.CharField(max_length=5)
+    ville = models.CharField(max_length=50)
+
+
